3cd695f0544d7f4265ca74bb204c9660ab59137d
Polarblau <polarblau@gmail.com>
2012-10-11 13:46:28 +0300

    Started overhaul: * Support pagination AND next/prev * More specific selectors * Event delegation * Access to transition options including callback

--- jquery.simpleslider.js
+++ jquery.simpleslider.js
@@ -1,60 +1,85 @@
 /*!
  * Simple Slider jQuery plugin
  *
- * Copyright (c) 2011 Florian Plank (http://www.polarblau.com/)
+ * Copyright (c) 2012 Florian Plank (http://www.polarblau.com/)
  * Dual licensed under the MIT (MIT-LICENSE.txt)
  * and GPL (GPL-LICENSE.txt) licenses.
  *
  * USAGE:
- *
+ * 
  * $('.simpleslider').simpleslider({
- *   "controls"           : $('.controls'),   // selector or jquery object
- *   "content"            : '.content',       // selector or jquery object
- *   "transitionDuration" : 500               // duration in ms
+ *   'pagination': '.pagination li',
+ *   'nextPage'  : 'a.next',
+ *   'prevPage'  : 'a.prev',
+ *   'content'   : '.content',
+ *   'slides'    : 'li',
+ *   'transition': {
+ *     'duration'  : 500,
+ *     'complete'  : function() {}
+ *   }
  * });
- *
+ * 
  */
 
 (function($) {
-	$.fn.simpleslider = function(options) {
+  $.fn.simpleslider = function(options) {
 
-		// defaults
-		var settings = $.merge({
-			"controls": '.controls',
-			"content": '.content',
-			"transitionDuration" : 500
-		}, options || {});
+    var settings = $.merge({
+      'pagination': '.pagination li',
+      'nextPage'  : 'a.next',
+      'prevPage'  : 'a.prev',
+      'content'   : '.content',
+      'slides'    : 'li',
+      'transition': {
+        'duration'  : 500,
+        'complete'  : function() {}
+      }
+    }, options || {});
 
-		return $(this).each(function(i, e) {
+    return this.each(function() {
 
-			var $e = $(e),
-			    $controls = $e.find('li', settings.controls),
-			    $content = $e.find(settings.content),
-			    sizes = [],
-			    contentWidth = 0;
+      var $this        = $(this),
+          $content     = $this.find(settings.content),
+          currentIndex = 0,
+          contentWidth = 0,
+          slidesSizes  = [];
 
-			// determine sizes of slides
-			$content.find('li').map(function(j, li) {
-				var liWidth = $(li).width();
-				contentWidth += liWidth;
-				sizes.push(liWidth);
-			})
-			$content.css('width', contentWidth + 'px');
+      slidesSizes = $content.find(settings.slides).map(function() {
+        return $(this).width();
+      });
 
-			$controls.click(function() {
-				// determine offset
-				var controlIndex = $(this).index(),
-					offset = (function(s) {
-					var w = 0; $.each(s, function(k, e) { w += e; }); return w;
-				})(sizes.slice(0, controlIndex));
+      contentWidth = sum(slidesSizes);
 
-				// slide content to correct position
-				$content.animate({
-					'left': (offset * -1) + 'px'
-				}, settings.transitionDuration);
-				return false;
-			});
-		});
+      $content.css('width', contentWidth + 'px')
 
-	};
+      $this
+        .on('click', settings.pagination, function(e) {
+          e.preventDefault()
+          slideTo($(this).index());
+        })
+        .on('click', settings.nextPage, function(e) {
+          e.preventDefault()
+          slideTo(currentIndex + 2 > slidesSizes.length ? 0 : currentIndex + 1);
+        })
+        .on('click', settings.prevPage, function(e) {
+          e.preventDefault()
+          slideTo(currentIndex - 1 < 0 ? slidesSizes.length - 1 : currentIndex - 1);
+        });
+
+      function slideTo(index) {
+        currentIndex = index;
+        var offset = sum(slidesSizes.slice(0, index));
+        $content.animate({
+          'left': (offset * -1) + 'px'
+        }, settings.transition);
+      }
+
+      function sum(list) {
+        var sum = 0;
+        $.each(list, function() { sum += this; });
+        return sum;
+      }
+
+    });
+  };
 })(jQuery);
